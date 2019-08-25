9a580e2c0251e4f3477ff66889aaee645fe26c18
Maxwell Connor <mconnor142@gmail.com>
2019-08-25 02:26:30 +0000

    Automatically backed up by Learn

--- objects.js
+++ objects.js
@@ -1,5 +1,6 @@
 var playlist = new Object({keys: '0'});
 
 function updatePlaylist(playlist, artistName, songTitle){
-  playlist[artistName] = 
+  playlist[artistName] = songTitle;
+  return playlist;
 }
